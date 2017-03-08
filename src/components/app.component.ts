756494b5010cbf5b532a137bbfe82b8ed4210ea3
Faouzi OUDOUH <faouzi.oudouh@gmail.com>
2017-03-08 14:49:03 +0000

    2.1: View encapsulation

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,5 +1,5 @@
 // Import Core
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 /**
  * For further details please check: https://angular.io/docs/ts/latest/api/core/index/Component-decorator.html
@@ -8,7 +8,11 @@ import {Component} from '@angular/core';
 
     // selector
     selector: 'app-workshop', // <app-workshop></app-workshop>
-    template: `<p class="big-font">My first angular component!!</p>`,
+    template: `<div class="test"><p class="big-font">My first angular component!!</p></div>`,
+    encapsulation: ViewEncapsulation.Emulated, // Default
+    // encapsulation: ViewEncapsulation.Native,
+    // encapsulation: ViewEncapsulation.None,
+
     // OR
     // templateUrl: 'url/to/template'
     styles: ['.big-font { font-size: 3em; }']
